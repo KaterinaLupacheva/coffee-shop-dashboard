db5e5bd16b77b5aec9b423c7a4f11ec15e7b8dee
Katerina <katerinalupacheva@gmail.com>
2020-04-21 22:31:50 +0300

    Expansion panel

--- src/pages/products.page.js
+++ src/pages/products.page.js
@@ -1,5 +1,12 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import {
+  Grid,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ProductCard from "../components/Cards/productCard.component";
 import AddNew from "../components/Cards/addNewCard.component";
 import { PRODUCTS } from "../data/products";
@@ -7,12 +14,21 @@ import { PRODUCTS } from "../data/products";
 const ProductsPage = () => {
   return (
     <>
-      {PRODUCTS.map((item, id) => (
-        <Grid item xs={12} sm={6} md={3} key={id}>
-          <ProductCard item={item} />
-        </Grid>
-      ))}
-      <AddNew />
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="h4">Hot Drinks</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Grid container spacing={3}>
+            {PRODUCTS.map((item, id) => (
+              <Grid item xs={12} sm={6} md={3} key={id}>
+                <ProductCard item={item} />
+              </Grid>
+            ))}
+            <AddNew />
+          </Grid>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </>
   );
 };
