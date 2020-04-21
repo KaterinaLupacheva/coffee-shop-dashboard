c1533f90d5241a0c1016f55c1cea06733a4b6118
Katerina <katerinalupacheva@gmail.com>
2020-04-21 14:54:36 +0300

    Pages skeleton added

---
+++ src/pages/dashboard.page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return <h1>DASHBOARD</h1>;
+};
+
+export default DashboardPage;
---
+++ src/pages/employees.page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmployeesPage = () => {
+  return <h1>EMPLOYEES</h1>;
+};
+
+export default EmployeesPage;
