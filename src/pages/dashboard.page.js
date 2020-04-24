3cb095f2dd3aa8264532eb317b792209eb9b673d
Katerina <katerinalupacheva@gmail.com>
2020-04-24 14:41:23 +0300

    Line chart component added

--- src/pages/dashboard.page.js
+++ src/pages/dashboard.page.js
@@ -1,7 +1,13 @@
 import React from "react";
+import LineChart from "../components/Charts/lineChart.component";
 
 const DashboardPage = () => {
-  return <h1>DASHBOARD</h1>;
+  return (
+    <>
+      <h1>DASHBOARD</h1>
+      <LineChart />
+    </>
+  );
 };
 
 export default DashboardPage;
